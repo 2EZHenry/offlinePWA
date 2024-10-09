f9b67e25941eedbc583caf44f6a1182f1f5da9b7
2EZHenry <113407743+2EZHenry@users.noreply.github.com>
2024-10-09 17:21:17 +0800

    test

--- service-worker.js
+++ service-worker.js
@@ -22,80 +22,59 @@ const putInCache = async (request, response) => {
 	await cache.put(request, response);
 };
 
-const cacheFirst = async ({ request, fallbackUrl }) => {
-	// First try to get the resource from the cache.
-	const responseFromCache = await caches.match(request);
-	if (responseFromCache) {
-		return responseFromCache;
+function createCacheBustedRequest(url) {
+	let request = new Request(url, { cache: "reload" });
+	// See https://fetch.spec.whatwg.org/#concept-request-mode
+	// This is not yet supported in Chrome as of M48, so we need to explicitly
+	// check to see if the cache: "reload" option had any effect.
+	if ("cache" in request) {
+		return request;
 	}
 
-	// If the response was not found in the cache,
-	// try to get the resource from the network.
-	try {
-		const responseFromNetwork = await fetch(request);
-		// If the network request succeeded, clone the response:
-		// - put one copy in the cache, for the next time
-		// - return the original to the app
-		// Cloning is needed because a response can only be consumed once.
-		putInCache(request, responseFromNetwork.clone());
-		return responseFromNetwork;
-	} catch (error) {
-		// If the network request failed,
-		// get the fallback response from the cache.
-		const fallbackResponse = await caches.match(fallbackUrl);
-		if (fallbackResponse) {
-			return fallbackResponse;
-		}
-		// When even the fallback response is not available,
-		// there is nothing we can do, but we must always
-		// return a Response object.
-		return new Response("Network error happened", {
-			status: 408,
-			headers: { "Content-Type": "text/plain" },
-		});
-	}
-};
+	// If { cache: "reload" } didn't have any effect, append a cache-busting URL
+	// parameter instead.
+	var bustedUrl = new URL(url, self.location.href);
+	bustedUrl.search += (bustedUrl.search ? "&" : "") + "cachebust=" + Date.now();
+	return new Request(bustedUrl);
+}
 
 self.addEventListener("fetch", (event) => {
 	// We only want to call event.respondWith() if this is a navigation request
 	// for an HTML page.
-	if (event.request.mode === "navigate") {
+	if (
+		event.request.mode === "navigate" ||
+		// request.mode of "navigate" is unfortunately not supported in Chrome
+		// versions older than 49, so we need to include a less precise fallback,
+		// which checks for a GET request with an Accept: text/html header.
+		(event.request.method === "GET" &&
+			event.request.headers.get("accept").indexOf("text/html") > -1)
+		// event.request.headers.get("accept").includes("text/html")
+	) {
+		console.log("Handling fetch event for", event.request.url);
 		event.respondWith(
-			(async () => {
-				try {
-					cacheFirst({
-						request: event.request,
-						fallbackUrl: "offline.html",
-					});
-					// First, try to use the navigation preload response if it's supported.
-					const preloadResponse = await event.preloadResponse;
-					if (preloadResponse) {
-						return preloadResponse;
-					}
-
-					const networkResponse = await fetch(event.request);
-					return networkResponse;
-				} catch (error) {
-					// catch is only triggered if an exception is thrown, which is likely
-					// due to a network error.
-					// If fetch() returns a valid HTTP response with a response code in
-					// the 4xx or 5xx range, the catch() will NOT be called.
-					console.log("Fetch failed; returning offline page instead.", error);
-
-					const cache = await caches.open(CACHE_NAME);
-					const cachedResponse = await cache.match(OFFLINE_URL);
-					return cachedResponse;
-				}
-			})()
+			fetch(createCacheBustedRequest(event.request.url)).catch((error) => {
+				// The catch is only triggered if fetch() throws an exception, which
+				// will most likely happen due to the server being unreachable.
+				// If fetch() returns a valid HTTP response with an response code in
+				// the 4xx or 5xx range, the catch() will NOT be called.
+				// If you need custom handling for 4xx or 5xx errors, see
+				// https://github.com/GoogleChrome/samples/tree/gh-pages/
+				// service-worker/fallback-response
+				alert("OFFLINE!");
+				console.log("Fetch failed; returning offline page instead.", error);
+				return caches.match("offline.html");
+			})
 		);
 	}
 
-	// If our if() condition is false, then this fetch handler won't intercept the
-	// request. If there are any other fetch handlers registered, they will get a
-	// chance to call event.respondWith(). If no fetch handlers call
-	// event.respondWith(), the request will be handled by the browser as if there
-	// were no service worker involvement.
+	// If our if() condition is false, then this fetch handler won"t intercept
+	// the request.
+	// If there are any other fetch handlers registered, they will get a chance
+	// to call event.respondWith().
+	// If no fetch handlers call event.respondWith(), the request will be
+	// handled by the browser as if there were no service worker involvement.
 });
+
 // Activate and clean up old caches
 self.addEventListener("activate", (event) => {
 	const cacheWhitelist = [CACHE_NAME];
