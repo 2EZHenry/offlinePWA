b4428d6cd53acefc2113b20189bd24c65b91e116
2EZHenry <113407743+2EZHenry@users.noreply.github.com>
2024-10-09 17:22:46 +0800

    prevent refresh

--- app.js
+++ app.js
@@ -14,3 +14,26 @@ if ("serviceWorker" in navigator) {
 			});
 	});
 }
+
+let touchStartY = 0;
+let isTouching = false;
+
+document.addEventListener("touchstart", function (e) {
+	// Record the start position of the touch
+	touchStartY = e.touches[0].clientY;
+	isTouching = true;
+});
+
+document.addEventListener("touchmove", function (e) {
+	if (isTouching) {
+		let touchMoveY = e.touches[0].clientY;
+		// If the user is pulling down and at the top of the page, prevent default behavior
+		if (touchMoveY > touchStartY && window.scrollY === 0) {
+			e.preventDefault(); // Prevent pull-to-refresh action
+		}
+	}
+});
+
+document.addEventListener("touchend", function () {
+	isTouching = false;
+});
