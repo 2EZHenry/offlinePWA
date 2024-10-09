f35b6a3cdc8afdd141000876fd62206796e0c861
2EZHenry <113407743+2EZHenry@users.noreply.github.com>
2024-10-09 17:56:49 +0800

    refactor: remove navigation check

--- service-worker.js
+++ service-worker.js
@@ -26,33 +26,31 @@ const putInCache = async (request, response) => {
 self.addEventListener("fetch", (event) => {
 	// We only want to call event.respondWith() if this is a navigation request
 	// for an HTML page.
-	if (event.request.mode === "navigate") {
-		event.respondWith(
-			(async () => {
-				try {
-					// First, try to use the navigation preload response if it's supported.
-					const preloadResponse = await event.preloadResponse;
-					if (preloadResponse) {
-						return preloadResponse;
-					}
-
-					const networkResponse = await fetch(event.request);
-					alert("NETWOKR!");
-					return networkResponse;
-				} catch (error) {
-					// catch is only triggered if an exception is thrown, which is likely
-					// due to a network error.
-					// If fetch() returns a valid HTTP response with a response code in
-					// the 4xx or 5xx range, the catch() will NOT be called.
-					console.log("Fetch failed; returning offline page instead.", error);
-					alert("FALLBACK!");
-					const cache = await caches.open(CACHE_NAME);
-					const cachedResponse = await cache.match(OFFLINE_URL);
-					return cachedResponse;
+	event.respondWith(
+		(async () => {
+			try {
+				// First, try to use the navigation preload response if it's supported.
+				const preloadResponse = await event.preloadResponse;
+				if (preloadResponse) {
+					return preloadResponse;
 				}
-			})()
-		);
-	}
+
+				const networkResponse = await fetch(event.request);
+				alert("NETWOKR!");
+				return networkResponse;
+			} catch (error) {
+				// catch is only triggered if an exception is thrown, which is likely
+				// due to a network error.
+				// If fetch() returns a valid HTTP response with a response code in
+				// the 4xx or 5xx range, the catch() will NOT be called.
+				console.log("Fetch failed; returning offline page instead.", error);
+				alert("FALLBACK!");
+				const cache = await caches.open(CACHE_NAME);
+				const cachedResponse = await cache.match(OFFLINE_URL);
+				return cachedResponse;
+			}
+		})()
+	);
 
 	// If our if() condition is false, then this fetch handler won't intercept the
 	// request. If there are any other fetch handlers registered, they will get a
